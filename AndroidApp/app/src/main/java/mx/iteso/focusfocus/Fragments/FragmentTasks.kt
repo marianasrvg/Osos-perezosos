60911aeebca4f5acc575619d603378337e8c4c16
DESKTOP-6P02NRP\Damian Garcia <damian95.gs@gmail.com>
2018-03-19 12:00:20 -0600

    add missing fragments to previous commit

---
+++ AndroidApp/app/src/main/java/mx/iteso/focusfocus/Fragments/FragmentTasks.kt
@@ -0,0 +1,71 @@
+package mx.iteso.focusfocus.Fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import mx.iteso.focusfocus.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [FragmentTasks.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [FragmentTasks.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FragmentTasks : Fragment() {
+
+    private var mListener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_tasks, container, false)
+    }
+
+        override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            mListener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+
+        // TODO: Rename and change types and number of parameters
+        fun newInstance(): FragmentTasks {
+            val fragment = FragmentTasks()
+            return fragment
+        }
+    }
+}// Required empty public constructor
